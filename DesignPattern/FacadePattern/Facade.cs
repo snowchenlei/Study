a4365be956e88bfd4fee9c742df6bc1ed48bb84e
陈磊 <gbdvjxu1257q@outlook.com>
2023-12-12 21:56:45 +0800

    外观模式

---
+++ DesignPattern/FacadePattern/Facade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern;
+
+public class Facade
+{
+    private readonly SubSystemOne _one;
+    private readonly SubSystemTwo _two;
+    private readonly SubSystemThree _three;
+
+    public Facade()
+    {
+        _one = new SubSystemOne();
+        _two = new SubSystemTwo();
+        _three = new SubSystemThree();
+    }
+    public void DoA()
+    {
+        Console.WriteLine("方法A————————");
+        _one.Do();
+        _two.Do();
+    }
+
+    public void DoB()
+    {
+        Console.WriteLine("方法B————————");
+        _two.Do();
+        _three.Do();
+    }
+}
---
+++ DesignPattern/FacadePattern/Program.cs
@@ -0,0 +1,9 @@
+﻿using FacadePattern;
+
+// 外观模式
+
+var facade = new Facade();
+facade.DoA();
+facade.DoB();
+
+Console.ReadKey();
