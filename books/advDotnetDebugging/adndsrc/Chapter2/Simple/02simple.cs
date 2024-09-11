67293b0ab0de35b5229f21b0b6be6aac472c4a37
snowchenlei <gbdvjxu1257q@outlook.com>
2024-09-11 13:49:53 +0800

    add advDotnetDebugging

---
+++ books/advDotnetDebugging/adndsrc/Chapter2/Simple/02simple.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Text;
+
+namespace Advanced.NET.Debugging.Chapter2
+{
+    class Simple
+    {
+        TypeSample(int x, int y, int z)
+        {
+            coordinates.x = x;
+            coordinates.y = y;
+            coordinates.z = z;
+        }
+
+        private struct Coordinates
+        {
+            public int x;
+            public int y;
+            public int z;
+        }
+
+        private Coordinates coordinates;
+
+        public void AddCoordinates()
+        {
+            int hashCode = GetHashCode();
+            lock (this)
+            {
+                Coordinates tempCoord;
+                tempCoord.x = coordinates.x + 100;
+                tempCoord.y = coordinates.y + 50;
+                tempCoord.z = coordinates.z + 100;
+
+                System.Console.WriteLine("x={0}, y={1}, z={2}", tempCoord.x, tempCoord.y, tempCoord.z);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Welcome to Advanced .NET Debugging!");
+        }
+    }
+}
