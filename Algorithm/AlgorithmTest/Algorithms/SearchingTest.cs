3127a4e2bbe7882034fb0560ddf4c5acd8f1908e
陈磊 <gbdvjxu1257q@outlook.com>
2024-09-29 17:02:50 +0800

    search

---
+++ Algorithm/AlgorithmTest/Algorithms/SearchingTest.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms;
+using Shouldly;
+
+namespace AlgorithmTest.Algorithms;
+
+public class SearchingTest
+{
+    [Theory]
+    [InlineData(new int[]{ 1, 2, 3 }, 2, 1)]
+    [InlineData(new int[] { 1, 2, 3 }, 3, 2)]
+    [InlineData(new int[] { 1, 2, 3 }, 1, 0)]
+    [InlineData(new int[] { 1, 2, 4, 3 }, 4, 2)]
+    [InlineData(new int[] { 1, 2, 4, 3 }, 2, 1)]
+    public void BinarySearch(int[] items, int target, int targetIndex)
+    {
+        int index = new Searching().BinarySearch(items, target);
+        index.ShouldBe(targetIndex);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3 }, 2, 1)]
+    [InlineData(new int[] { 1, 2, 2, 2, 3 }, 2, 1)]
+    public void BinarySearchInsertion(int[] items, int target, int targetIndex)
+    {
+        int index = new Searching().BinarySearchInsertion(items.ToList(), target);
+        index.ShouldBe(targetIndex);
+    }
+}
---
+++ Algorithm/Algorithms/Searching.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics;
+
+namespace Algorithms;
+
+public class Searching
+{
+    public int BinarySearch<T>(IList<T> items, T target) where T : IComparisonOperators<T, T, bool>
+    {
+        int i = 0, j = items.Count() - 1;
+        while (i <= j)
+        {
+            int m = (i + j) / 2;
+            T data = items[m];
+            if (data < target)
+            {
+                i = m + 1;
+            }
+            else if (data > target)
+            {
+                j = m - 1;
+            }
+            else
+            {
+                return m;
+            }
+        }
+
+        return -1;
+    }
+
+    public int BinarySearchInsertion<T>(List<T> items, T target) where T : IComparisonOperators<T, T, bool>
+    {
+        int i = 0, j = items.Count() - 1;
+        while (i <= j)
+        {
+            int m = (i + j) / 2;
+            T data = items[m];
+            if (data < target)
+            {
+                i = m + 1;
+            }
+            else if (data > target)
+            {
+                j = m - 1;
+            }
+            else
+            {
+                for (int k = m; k > 0; k--)
+                {
+                    if (items[k] != target)
+                    {
+                        break;
+                    }
+                    m = k;
+
+                }
+                items.Insert(m, target);
+                return m;
+            }
+        }
+
+        return -1;
+    }
+}
