ddfedf0831d6124bf421f444bea0f5c6b9b95465
陈磊 <gbdvjxu1257q@outlook.com>
2024-09-29 17:25:27 +0800

    SelectionSort

--- Algorithm/AlgorithmTest/Algorithms/SearchingTest.cs
+++ Algorithm/AlgorithmTest/Algorithms/SearchingTest.cs
@@ -11,7 +11,7 @@ public class SearchingTest
     [InlineData(new int[] { 1, 2, 3 }, 1, 0)]
     [InlineData(new int[] { 1, 2, 4, 3 }, 4, 2)]
     [InlineData(new int[] { 1, 2, 4, 3 }, 2, 1)]
-    public void BinarySearch(int[] items, int target, int targetIndex)
+    public void BinarySearchTest(int[] items, int target, int targetIndex)
     {
         int index = new Searching().BinarySearch(items, target);
         index.ShouldBe(targetIndex);
@@ -20,7 +20,7 @@ public class SearchingTest
     [Theory]
     [InlineData(new int[] { 1, 2, 3 }, 2, 1)]
     [InlineData(new int[] { 1, 2, 2, 2, 3 }, 2, 1)]
-    public void BinarySearchInsertion(int[] items, int target, int targetIndex)
+    public void BinarySearchInsertionTest(int[] items, int target, int targetIndex)
     {
         int index = new Searching().BinarySearchInsertion(items.ToList(), target);
         index.ShouldBe(targetIndex);
---
+++ Algorithm/AlgorithmTest/Algorithms/SortingTest.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms;
+using Shouldly;
+using Xunit.Abstractions;
+
+namespace AlgorithmTest.Algorithms;
+
+public class SortingTest
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 8, 2, 4, 2, 3, 9, 16, 25 })]
+    public void SelectionSortTest(int[] items)
+    {
+        new Sorting().SelectionSort(items);
+        items.Length.ShouldBe(items.Length);
+        items[0].ShouldBe(items.Min());
+        items[^1].ShouldBe(items.Max());
+    }
+
+    [Fact]
+    public void SelectionSort_Empty()
+    {
+        int[] items = [];
+        new Sorting().SelectionSort(items);
+        items.Length.ShouldBe(0);
+    }
+}
