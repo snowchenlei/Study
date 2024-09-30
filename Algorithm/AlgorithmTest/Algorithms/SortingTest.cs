32046f647438e4ffc248e5487a715dbd155bbb18
snowchenlei <gbdvjxu1257q@outlook.com>
2024-09-30 17:24:28 +0800

    add sorting

--- Algorithm/AlgorithmTest/Algorithms/SortingTest.cs
+++ Algorithm/AlgorithmTest/Algorithms/SortingTest.cs
@@ -24,4 +24,61 @@ public class SortingTest
         new Sorting().SelectionSort(items);
         items.Length.ShouldBe(0);
     }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 8, 2, 4, 2, 3, 9, 16, 25 })]
+    public void BubbleSortTest(int[] items)
+    {
+        new Sorting().BubbleSort(items);
+        items.Length.ShouldBe(items.Length);
+        items[0].ShouldBe(items.Min());
+        items[^1].ShouldBe(items.Max());
+    }
+
+    [Fact]
+    public void BubbleSortTest_Empty()
+    {
+        int[] items = [];
+        new Sorting().SelectionSort(items);
+        items.Length.ShouldBe(0);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 8, 2, 4, 2, 3, 9, 16, 25 })]
+    public void BubbleSortWithFlagTest(int[] items)
+    {
+        new Sorting().BubbleSortWithFlag(items);
+        items.Length.ShouldBe(items.Length);
+        items[0].ShouldBe(items.Min());
+        items[^1].ShouldBe(items.Max());
+    }
+
+    [Fact]
+    public void BubbleSortWithFlagTest_Empty()
+    {
+        int[] items = [];
+        new Sorting().BubbleSortWithFlag(items);
+        items.Length.ShouldBe(0);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4, 5 })]
+    [InlineData(new int[] { 8, 2, 4, 2, 3, 9, 16, 25 })]
+    public void InsertionSortTest(int[] items)
+    {
+        new Sorting().InsertionSort(items);
+        items.Length.ShouldBe(items.Length);
+        items[0].ShouldBe(items.Min());
+        items[^1].ShouldBe(items.Max());
+    }
+
+    [Fact]
+    public void InsertionSortTest_Empty()
+    {
+        int[] items = [];
+        new Sorting().InsertionSort(items);
+        items.Length.ShouldBe(0);
+    }
 }
